e70e54d7c59aa398b77986d40f3817027b376a22
xiaoguiking <king1129371217@163.com>
2020-06-05 22:50:37 +0800

    代码规范

--- react-bicycle/src/components/NavLeft/index.js
+++ react-bicycle/src/components/NavLeft/index.js
@@ -1,10 +1,67 @@
 import React, { Component } from "react";
+import MenuConfig from "../../config/menuConfig";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+import MenuItem from "antd/lib/menu/MenuItem";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+
+function handleClick(e) {
+  console.log("click", e);
+}
+
+const getNavNode2 = (MenuConfig) => {
+  return MenuConfig.reduce((pre, item) => {
+    //添加Menu.item
+    if (!item.children) {
+      pre.push(
+        <MenuItem key={item.key}>
+          <Link to={item.key}>
+            {/* <Icon type={item.icon} /> */}
+            <span>{item.title}</span>
+          </Link>
+        </MenuItem>
+      );
+    } else {
+      //添加SubMenu
+      pre.push(
+        <SubMenu
+          key={item.key}
+          title={
+            <span>
+              {/* <Icon type={item.icon} /> */}
+              <span>{item.title}</span>
+            </span>
+          }
+        >
+          {getNavNode2(item.children)}
+        </SubMenu>
+      );
+    }
+    return pre;
+  }, []);
+};
 
 export default class extends Component {
   render() {
-    const style = {
-      backgroundColor: "red",
-    };
-    return <div style={style}>navLeft</div>;
+    // const style = {
+    //   backgroundColor: "red",
+    // };
+    return (
+      <div>
+        <div className="logo">
+          logo
+          <img src="" alt="logo" />
+        </div>
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          {getNavNode2(MenuConfig)}
+        </Menu>
+      </div>
+    );
   }
 }
