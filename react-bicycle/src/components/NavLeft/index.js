e2ed09120d9ba557e7b8cf64dca97bcf23f4be56
xiaoguiking <king1129371217@163.com>
2020-06-05 23:28:06 +0800

    侧边栏

--- react-bicycle/src/components/NavLeft/index.js
+++ react-bicycle/src/components/NavLeft/index.js
@@ -1,65 +1,75 @@
-import React, { Component } from "react";
-import MenuConfig from "../../config/menuConfig";
-import { Menu } from "antd";
-import { Link } from "react-router-dom";
-import MenuItem from "antd/lib/menu/MenuItem";
-import {
-  MailOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
+import React, { Component } from 'react';
+import MenuConfig from '../../config/menuConfig';
+import { Menu } from 'antd';
+import { Link } from 'react-router-dom';
+import MenuItem from 'antd/lib/menu/MenuItem';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import './index.less';
 
 const { SubMenu } = Menu;
 
 function handleClick(e) {
-  console.log("click", e);
+  console.log('click', e);
 }
 
-const getNavNode2 = (MenuConfig) => {
-  return MenuConfig.reduce((pre, item) => {
-    //添加Menu.item
-    if (!item.children) {
-      pre.push(
-        <MenuItem key={item.key}>
-          <Link to={item.key}>
+export default class extends Component {
+  // 菜单渲染
+  renderNavNode2 = (MenuConfig) => {
+    console.log(MenuConfig, '123');
+    return MenuConfig.reduce((pre, item) => {
+      //添加Menu.item
+      if (!item.children) {
+        pre.push(
+          <MenuItem key={item.key}>
+            {/* <Link to={item.key}> */}
             {/* <Icon type={item.icon} /> */}
             <span>{item.title}</span>
-          </Link>
-        </MenuItem>
-      );
-    } else {
-      //添加SubMenu
-      pre.push(
-        <SubMenu
-          key={item.key}
-          title={
-            <span>
-              {/* <Icon type={item.icon} /> */}
-              <span>{item.title}</span>
-            </span>
-          }
-        >
-          {getNavNode2(item.children)}
-        </SubMenu>
-      );
-    }
-    return pre;
-  }, []);
-};
+            {/* </Link> */}
+          </MenuItem>,
+        );
+      } else {
+        //添加SubMenu
+        pre.push(
+          <SubMenu
+            key={item.key}
+            title={
+              <span>
+                {/* <Icon type={item.icon} /> */}
+                <span>{item.title}</span>
+              </span>
+            }
+          >
+            {this.renderNavNode2(item.children)}
+          </SubMenu>,
+        );
+      }
+      return pre;
+    }, []);
+  };
 
-export default class extends Component {
+  // componentDidMount() {
+  //   const menuTreeNode = this.renderNavNode2(MenuConfig);
+  //   this.setState({
+  //     menuTreeNode,
+  //   });
+  // }
+  componentWillMount() {
+    const menuTreeNode = this.renderNavNode2(MenuConfig);
+    this.setState({
+      menuTreeNode,
+    });
+  }
   render() {
-    // const style = {
-    //   backgroundColor: "red",
-    // };
     return (
       <div>
         <div className="logo">
-          logo
-          <img src="" alt="logo" />
+          <img src="/assets/logo-ant.svg" alt="logo" />
+          <h1>logo</h1>
         </div>
-        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-          {getNavNode2(MenuConfig)}
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          {this.state.menuTreeNode}
+          {/* <Menu.Item key="setting:1">Option 1</Menu.Item>
+          <Menu.Item key="setting:2">Option 2</Menu.Item> */}
         </Menu>
       </div>
     );
